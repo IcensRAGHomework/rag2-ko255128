880675ba73bfcc36d0379931621e99c1862c85b9
ChunHsiang Chang <chunhsiang_chang@asus.com>
2025-01-23 13:15:50 +0800

    HW2 Version 3

--- student_assignment.py
+++ student_assignment.py
@@ -17,16 +17,20 @@ def hw02_1(q1_pdf):
 def hw02_2(q2_pdf):
     loader = PyPDFLoader(q2_pdf)
     docs = loader.load()
+    texts= ""
+    for doc in docs:
+        texts = texts + doc.page_content
     # print(docs)
     textSplitter = RecursiveCharacterTextSplitter(separators=["\n   第 .* 章","\n第 \\d* 條","\n第 \\d*-\\d* 條"], chunk_size=12 ,chunk_overlap=0, is_separator_regex=True)
-    chunks = textSplitter.split_documents(docs)
-    return len(chunks)
+    chunks = textSplitter.split_text(texts)
+    return chunks
 
 
 # if __name__ == "__main__":
 #     response = hw02_2(q2_pdf)
 #     print ("lenth:", len(response))
 #     for element in response:
-#         print(element.metadata)
-#         print(element.page_content)
+#         print("--")
+#         print(element)
+#         print("--")
 #         print()
